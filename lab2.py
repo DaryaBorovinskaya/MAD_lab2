5f9e52a342dd6677fbb9c58f0a6604f9f847c7d5
DaryaBorovinskaya <dashhaha17@gmail.com>
2025-10-14 09:36:44 +0700

    Загрузка датасета

--- lab2.py
+++ lab2.py
@@ -1,1 +1,16 @@
-#meow
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+
+file_path = './online_shoppers_intention.csv'
+
+
+df = pd.read_csv(file_path)
+
+print(f"Размер: {df.shape}")
+
+print(df.describe())
+
+
+
+
+
